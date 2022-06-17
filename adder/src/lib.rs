c9e95fd3dc869c7425996365805a9f9dac77f6cf
Aoi <ht.so.ik0102@gmail.com>
2022-06-17 10:15:00 +0700

    Pass guess test

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -10,6 +10,20 @@ impl Rectangle {
     }
 }
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+
+        Guess { value }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -45,6 +59,12 @@ mod tests {
     #[test]
     fn exploration() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    #[should_panic]
+    fn greeter_than_100() {
+        Guess::new(200);
     }
 
     // #[test]
