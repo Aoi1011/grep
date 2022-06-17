304ca53e504557c0798058319d0a72bffb2b4d7f
Aoi <ht.so.ik0102@gmail.com>
2022-06-17 10:48:28 +0700

    Controlling how tess are run

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -17,16 +17,54 @@ pub struct Guess {
 impl Guess {
     pub fn new(value: i32) -> Guess {
         if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {}", value);
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}",
+                value
+            );
         }
 
         Guess { value }
     }
 }
 
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    4
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(value, 4);
+    }
+
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
+    }
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
 
     #[test]
     fn larger_can_hold_smaller() {
@@ -62,11 +100,31 @@ mod tests {
     }
 
     #[test]
-    #[should_panic]
-    fn greeter_than_100() {
-        Guess::new(200);
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
     }
 
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
+    }
+
+    // #[test]
+    // #[should_panic]
+    // fn greeter_than_100() {
+    //     Guess::new(200);
+    // }
+
+    // #[test]
+    // #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    // fn greater_than_100() {
+    //     Guess::new(200);
+    // }
     // #[test]
     // fn another() {
     //     panic!("Make this test fail");
