39c6776e4070402fc9ee8480903722cb3f219729
Aoi <ht.so.ik0102@gmail.com>
2022-06-19 10:47:56 +0700

    Patterns and Matching

---
+++ patterns-matching/src/lib.rs
@@ -0,0 +1,53 @@
+enum Color {
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(Color),
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Color, Message};
+
+    #[test]
+    fn it_works() {
+        let favorite_color: Option<&str> = None;
+        let is_tuesday = false;
+        let age: Result<u8, _> = "34".parse();
+
+        if let Some(color) = favorite_color {
+            println!("Using your favorite color, {}, as the background", color);
+        } else if is_tuesday {
+            println!("Tuesday is green day!");
+        } else if let Ok(age) = age {
+            if age > 30 {
+                println!("Using purple as the background color");
+            } else {
+                println!("Using orange as the background color");
+            }
+        } else {
+            println!("Using blue as the background color");
+        }
+    }
+
+    #[test]
+    fn it_works_pattern() {
+        let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+
+        match msg {
+            Message::ChangeColor(Color::Rgb(r, g, b)) => {
+                println!("Change the color to red {}, green {}, and blue {}", r, g, b)
+            }
+            Message::ChangeColor(Color::Hsv(h, s, v)) => println!(
+                "Change the color to hue {}, saturation {}, and value {}",
+                h, s, v
+            ),
+            _ => (),
+        }
+    }
+}
